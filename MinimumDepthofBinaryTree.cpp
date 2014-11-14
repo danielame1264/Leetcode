1897a80a06a304837e0e6c8db4bcd35310d47706
danielame1264 <danielame1264@gmail.com>
2014-11-13 22:42:49 -0500

    Update MinimumDepthofBinaryTree.cpp

--- MinimumDepthofBinaryTree.cpp
+++ MinimumDepthofBinaryTree.cpp
@@ -45,3 +45,51 @@ public:
         return INT32_MAX;  // actually won't come here?
     }
 };
+
+
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        int minDepth = 1;
+        int currentLevelCount = 0;
+        int nextLevelCount = 0;
+        
+        LinkedList<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        currentLevelCount++;
+        while(!q.isEmpty()){
+            TreeNode cur = q.remove();
+            currentLevelCount--;
+            
+            if(cur.left!=null){
+                nextLevelCount ++;
+                q.add(cur.left);
+            }
+            
+            if(cur.right!=null){
+                nextLevelCount ++;
+                q.add(cur.right);
+            }
+            if(cur.right==null && cur.left==null){
+                return minDepth;
+            }
+            
+            if(currentLevelCount==0){
+                minDepth++;
+                currentLevelCount=nextLevelCount;
+                nextLevelCount=0;
+            }
+            
+        }
+        return Integer.MAX_VALUE;
+    }
+}
