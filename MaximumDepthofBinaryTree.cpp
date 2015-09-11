e5b0a5123537c7f8c3f32008f5a3d7478cd545e5
danielame1264 <danielame1264@gmail.com>
2015-09-11 00:04:06 -0400

    Update MaximumDepthofBinaryTree.cpp

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -40,3 +40,58 @@ public:
     }
  */   
 };
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        # this is top down. Quite a lot of duplicated calculation
+        # if not root: return 0
+        # else:
+        #     return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+        # of course you can do bfs
+        # from collections import deque
+        # if not root: return 0
+        # depth = 0
+        # this = deque([root])
+        # next = deque([])
+
+        # while True:
+        #     while len(this)>0:
+        #         current = this.popleft()
+        #         if current.right: next.append(current.right)
+        #         if current.left: next.append(current.left)
+        #     depth+=1
+        #     if not next: break
+        #     else:
+        #         this = next
+        #         next = deque([])
+        # return depth
+        
+        if root == None:
+            return 0
+        nodeStack = [root];
+        depthStack = [1];
+        maxDepth = 0;
+        while len(nodeStack)>0:
+            node = nodeStack.pop();
+            depth = depthStack.pop();
+            maxDepth = maxDepth if maxDepth > depth else depth
+            if node.left != None:
+                nodeStack.append(node.left)
+                depthStack.append(depth+1)
+            if node.right != None:
+                nodeStack.append(node.right)
+                depthStack.append(depth+1)
+        return maxDepth
